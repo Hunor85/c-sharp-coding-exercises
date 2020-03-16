acee1d232b089a803941100c73ce1be07012f0a9
Hunor Vadasz-Perhat <hunor.vadasz@gmail.com>
2020-03-16 11:18:57 +0100

    init

---
+++ algorithms/searching/no-02-linear-search/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace no_02_linear_search
+{
+    class Program
+    {
+        public static int search(int[] arr, int x)
+        {
+            int n = arr.Length;
+            for (int i = 0; i < n; i++)
+            {
+                if (arr[i] == x) {
+                    Console.WriteLine("Index: " + i + " Element: " + arr[i]);
+                    return i;
+                }              
+            }
+            return -1;
+        }
+
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 3, 4, 5 };
+            int x = 3;
+
+            int result = search(arr, x);
+
+            if (result == -1)
+                Console.WriteLine("Element is not present in array");
+            else
+                Console.WriteLine("Element is present at index " + result);
+        }
+    }
+}
