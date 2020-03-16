74dae46f42ca37b940194a18a94a5b7a28ddf447
Hunor Vadasz-Perhat <hunor.vadasz@gmail.com>
2020-03-16 14:15:14 +0100

    init

---
+++ algorithms/searching/no-03-binary-search/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace no_03_binary_search
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
